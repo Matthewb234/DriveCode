0cc484b7ce6166b1d34b2da694ac4b968b25229c
matthew <you@example.com>
2019-01-31 16:55:09 -0800

    Jimmy forced me.

--- src/main/java/com/matthew/driveCodes/TurnAndThrottleDrive.java
+++ src/main/java/com/matthew/driveCodes/TurnAndThrottleDrive.java
@@ -5,38 +5,38 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class TurnAndThrottleDrive {
-	private Joystick throttle = new Joystick(2);
-	private Joystick wheel = new Joystick(0);
-	private TalonSRX rightMaster = new TalonSRX(1);
-	private TalonSRX leftMaster = new TalonSRX(0);
-	private double rightPower;
-	private double leftPower;
-	private double outsidePower;
-	private double insidePower;
+    private Joystick throttle = new Joystick(2);
+    private Joystick wheel = new Joystick(0);
+    private TalonSRX rightMaster = new TalonSRX(1);
+    private TalonSRX leftMaster = new TalonSRX(0);
+    private double rightPower;
+    private double leftPower;
+    private double outsidePower;
+    private double insidePower;
 
-	public void tTPeriodic(){
-		outsidePower = throttle.getRawAxis(1);
-		if(wheel.getRawAxis(0) <= 0) {
-			insidePower = (1 + wheel.getRawAxis(0)) * throttle.getRawAxis(1);
-		}else if(wheel.getRawAxis(0) >= 0){
-			insidePower = (1 - wheel.getRawAxis(0)) * throttle.getRawAxis(1);
-		}
-		calculatePower();
-		rightMaster.set(ControlMode.PercentOutput, rightPower);
-		leftMaster.set(ControlMode.PercentOutput, leftPower);
-	}
+    public void tTPeriodic() {
+        outsidePower = throttle.getRawAxis(1);
+        if (wheel.getRawAxis(0) <= 0) {
+            insidePower = (1 + wheel.getRawAxis(0)) * throttle.getRawAxis(1);
+        } else if (wheel.getRawAxis(0) >= 0) {
+            insidePower = (1 - wheel.getRawAxis(0)) * throttle.getRawAxis(1);
+        }
+        calculatePower();
+        rightMaster.set(ControlMode.PercentOutput, rightPower);
+        leftMaster.set(ControlMode.PercentOutput, leftPower);
+    }
 
-	public void calculatePower() {
-		System.out.println("hi");
-		if(wheel.getRawAxis(0) >= 0) {
-			leftPower = insidePower;
-			rightPower = outsidePower;
-		
-		}else {
-			leftPower = outsidePower;
-			rightPower = insidePower;
-			
-		}
-	}
+    public void calculatePower() {
+        System.out.println("hi");
+        if (wheel.getRawAxis(0) >= 0) {
+            leftPower = insidePower;
+            rightPower = outsidePower;
+
+        } else {
+            leftPower = outsidePower;
+            rightPower = insidePower;
+
+        }
+    }
 }
 
